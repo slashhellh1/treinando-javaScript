0c7d3442c73ee5fd53052362f1973b1b165b9ef3
slashhellh1 <slashhell@hotmail.com>
2023-11-16 22:21:16 -0300

    feat: start in the JavaScript

---
+++ index.js
@@ -0,0 +1,54 @@
+//console é um objeto que atraaves dos seus metodos
+// faz com que o programador interaja com a saida do ambiente que 
+// o javaScript está sendo executado.
+
+// console.log('olá Mundo !!!!!')
+// console.warn('ateção')
+// console.error('ERRO')
+// console.info('Informação')
+
+// let fruits = ['apple','banana','orange']
+
+// console.table(fruits);
+
+//console.clear();
+
+// variaveis
+
+var a = 1
+console.log(a)
+
+let b = 'isael'
+console.log(b)
+
+const c = true
+console.log(c)
+
+let d 
+console.log(d)
+
+let e = null
+console.log(e)
+
+const fruits = ['apple','banana','orange']
+console.log(fruits)
+
+const number = [10 , 20, 30]
+console.log(number)
+
+const user = {name : 'isael silva', age: 29}
+console.log(user)
+
+const arrayUsers = [
+    {
+        name: 'alfredo', age: 55
+    },
+    {
+        name: 'cassio', age: 15
+    }
+]
+console.log(arrayUsers)
+
+let userOne = arrayUsers[0].age
+console.log(userOne, 'estou aqui')
+
---
+++ treinando.js
@@ -0,0 +1,26 @@
+// AT1 - Criar variáveis com os tipos que aprendemos
+
+// AT2 - Colocar todos os consoles que aprendemos
+
+var idade = 38
+console.log(idade)
+
+
+let nome = 'Robson'
+console.log(nome)
+
+const morte = true
+console.log(morte)
+
+let diogo
+console.log(diogo)
+
+let nulo = null
+console.log(nulo)
+
+const videoGames = ['nintendo', 'nintendo 64', 'nintendo swith']
+console.log(videoGames)
+
+var idade = 39
+console.log(idade)
+
